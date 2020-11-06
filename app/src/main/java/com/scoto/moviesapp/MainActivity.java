e6d18307be6e2ea98b9292c756488a1fd1b13d17
scoto <scotoglu@gmail.com>
2020-11-06 23:49:31 +0300

    SearchView ve Hamburger-icon eklendi.

--- app/src/main/java/com/scoto/moviesapp/MainActivity.java
+++ app/src/main/java/com/scoto/moviesapp/MainActivity.java
@@ -1,12 +1,15 @@
 package com.scoto.moviesapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -14,10 +17,11 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
+    private static final String TAG = "MainActivity";
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +32,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.activity_main_nav_view);
         toolbar = findViewById(R.id.toolbar);
+        searchView = findViewById(R.id.search_view);
 
+        this.configurationSearchView();
         this.configurationToolbar();
         this.configurationDrawerLayout();
         this.configurationNavigationView();
+
+    }
+
+    private void configurationSearchView() {
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchView.setIconified(false);
+            }
+        });
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d(TAG, "onQueryTextChange: Text is: " + newText);
+                //TODO --> call filter method or get result
+                return true;
+            }
+        });
+
 
     }
 
@@ -42,6 +72,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private void configurationDrawerLayout() {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
         drawerLayout.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(false);
+        toggle.setToolbarNavigationClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        toggle.setHomeAsUpIndicator(R.mipmap.hamburger_ic);
         toggle.syncState();
     }
 
